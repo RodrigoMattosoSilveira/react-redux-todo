da7e8f58399e62d85decf0735f6fd0fb95eb7166
Rodrigo Silveira <r3850365@gmail.com>
2020-04-18 03:20:40 -0700

    minor_style_changes  -  style: filter selectors are a tiny bit more elegant

--- src/components/visibility-filters.tsx
+++ src/components/visibility-filters.tsx
@@ -6,22 +6,35 @@ import { VISIBILITY_TYPES } from '../reference/visibility-types';
 // Import interfaces
 import { VisibilityFiltersInterface } from './../interfaces'
 
-function computeClassNames (myVisibilityFilter: string, propVisibilityFilter: string ): string {
-    console.log('test');
-    let classNames = 'filter';
-    if (myVisibilityFilter === propVisibilityFilter) {
-        classNames += ', filter-active';
-    }
-    return classNames;
+function computeClassNames (myVisibilityFilter: string): string {
+    return 'btn ' + myVisibilityFilter;
+}
+
+function isDisabled (myVisibilityFilter: string, propVisibilityFilter: string ): boolean {
+
+    return myVisibilityFilter === propVisibilityFilter ? true : false;
 }
 
 // TodoList component
 const VisibilityFilters = (props: VisibilityFiltersInterface) => {
   return (
     <div className="visibility-filter">
-        <span className={computeClassNames(VISIBILITY_TYPES.ALL, props.visibilityFilter)} onClick={() => props.handleVisibilityFilter(VISIBILITY_TYPES.ALL)}>{VISIBILITY_TYPES.ALL}</span>
-        <span className={computeClassNames(VISIBILITY_TYPES.OPEN, props.visibilityFilter)} onClick={() => props.handleVisibilityFilter(VISIBILITY_TYPES.OPEN)}>{VISIBILITY_TYPES.OPEN}</span>
-        <span className={computeClassNames(VISIBILITY_TYPES.DONE, props.visibilityFilter)} onClick={() => props.handleVisibilityFilter(VISIBILITY_TYPES.DONE)}>{VISIBILITY_TYPES.DONE}</span>
+        <span>Show: </span>
+        <button
+            className={computeClassNames(VISIBILITY_TYPES.ALL)}
+            disabled={isDisabled(VISIBILITY_TYPES.ALL, props.visibilityFilter)}
+            onClick={() => props.handleVisibilityFilter(VISIBILITY_TYPES.ALL)}>{VISIBILITY_TYPES.ALL}
+        </button>
+        <button
+            className={computeClassNames(VISIBILITY_TYPES.OPEN)}
+            disabled={isDisabled(VISIBILITY_TYPES.OPEN, props.visibilityFilter)}
+            onClick={() => props.handleVisibilityFilter(VISIBILITY_TYPES.OPEN)}>{VISIBILITY_TYPES.OPEN}
+        </button>
+        <button
+            className={computeClassNames(VISIBILITY_TYPES.DONE)}
+            disabled={isDisabled(VISIBILITY_TYPES.DONE, props.visibilityFilter)}
+            onClick={() => props.handleVisibilityFilter(VISIBILITY_TYPES.DONE)}>{VISIBILITY_TYPES.DONE}
+        </button>
     </div>
   )
 }
