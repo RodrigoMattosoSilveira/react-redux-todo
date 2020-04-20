bee47ff1ae57d7fb097869b4ebff1d427738e784
Rodrigo Silveira <r3850365@gmail.com>
2020-04-20 16:24:44 -0700

    static_typing  -  refactor: static typed todo-form

--- src/components/todo-form.tsx
+++ src/components/todo-form.tsx
@@ -1,49 +1,64 @@
 // Import dependencies
 import * as React from 'react';
-import shortid from 'shortid';
 
 // Import interfaces
-import {TodoInterface, TodoFormInterface} from './../interfaces/interfaces';
+import { todo_add } from '../actions/todo-actions';
+import { connect, ConnectedProps } from 'react-redux'
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+interface Props extends PropsFromRedux {
+}
+
+const mapStateToProps = null;
+
+const mapDispatchToProps = {
+	todo_add: (text: string) => todo_add(text)
+}
+
+const connector = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)
 
 // Todo form component
-const TodoForm = (props: TodoFormInterface) => {
-  // Create ref for form input
-  const inputRef = React.useRef<HTMLInputElement>(null)
-  // Create form state
-  const [formState, setFormState] = React.useState('')
-  // Handle todo input change
-  function handleInputOnChange(event: React.ChangeEvent<HTMLInputElement>) {
-    // Update form state with the text from input
-    setFormState(event.target.value)
-  }
-  // Handle 'Enter' in todo input
-  function handleInputOnKeyPress(event: React.KeyboardEvent) {
-    // Check for 'Enter' key
-    if (event.key === 'Enter') {
-      // Prepare new todo object
-      const newTodo: TodoInterface = {
-        id: shortid.generate(),
-        text: formState,
-        isCompleted: false
-      }
-      // Create new todo item
-      props.handleTodoCreate(newTodo)
-      // Reset the input field
-      if (inputRef && inputRef.current) {
-        inputRef.current.value = '';
-      }
-    }
-  }
-  return (
-    <div className="todo-form">
-      <input
-        ref={inputRef}
-        type="text"
-        placeholder='Enter new todo'
-        onChange={event => handleInputOnChange(event)}
-        onKeyPress={event => handleInputOnKeyPress(event)}
-      />
-    </div>
-  )
+const TodoForm = (props: Props) => {
+	// Create ref for form input
+	const inputRef = React.useRef<HTMLInputElement>(null);
+	
+	// Create form state
+	const [formState, setFormState] = React.useState('');
+	
+	// Handle todo input change
+	function handleInputOnChange(event: React.ChangeEvent<HTMLInputElement>) {
+		// Update form state with the text from input
+		setFormState(event.target.value)
+	}
+	
+	
+	// Handle 'Enter' in todo input
+	function handleInputOnKeyPress(event: React.KeyboardEvent) {
+		// Check for 'Enter' key
+		if (event.key === 'Enter') {
+			// Dispatch
+			props.todo_add(formState)
+			
+			// Reset the input field
+			if (inputRef && inputRef.current) {
+				inputRef.current.value = '';
+			}
+		}
+	}
+	return (
+		<div className="todo-form">
+			<input
+				ref={inputRef}
+				type="text"
+				placeholder='Enter new todo'
+				onChange={event => handleInputOnChange(event)}
+				onKeyPress={event => handleInputOnKeyPress(event)}
+			/>
+		</div>
+	)
 }
-export default TodoForm;
+
+export default connector(TodoForm)
