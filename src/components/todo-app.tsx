11c659260211005d22e1837ee6760c6e74595e67
Rodrigo Silveira <r3850365@gmail.com>
2020-04-21 15:42:22 -0700

    material_ui  -  refactor: Replaced our todo with a Material one

--- src/components/todo-app.tsx
+++ src/components/todo-app.tsx
@@ -1,6 +1,14 @@
 // External Dependencies
 import * as React from "react";
 import {connect, useSelector} from "react-redux";
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+// import MenuIcon from '@material-ui/icons/Menu';
+
 
 // Internal Dependencies
 import {RootState} from "../reducers/rooReducer";
@@ -9,14 +17,35 @@ import TodoList from "./todo-list";
 import VisibilityFilters from "./visibility-filters";
 import "../styles/styles.css";
 
+// import classes from "*.module.scss";
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+}));
 
 const TodoApp = () => {
 	const todoList  = useSelector((state: RootState) => state.todo_reducer);
 	const visibilityFilter  = useSelector((state: RootState) => state.visibility_filter_reducer);
-	
+	const classes = useStyles();
 	return (
 		<div className="todo-list-app">
-			<h1 className="todo-header">todo</h1>
+			{/* <h1 className="todo-header">todo</h1>*/}
+			<div className={classes.root}>
+				<AppBar position="static">
+					<Toolbar>
+						<Typography variant="h5" className={classes.title}>
+							React Redux TS Material UI Todo List
+						</Typography>
+					</Toolbar>
+				</AppBar>
+			</div>
 			<TodoForm />
 			<TodoList
 				todoList={todoList}
