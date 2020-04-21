60906febf780bc9715e0d5f1d8c09278dc148887
Rodrigo Silveira <r3850365@gmail.com>
2020-04-20 21:31:54 -0700

    static_typing  -  refactor: static typed todo-list

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,11 +1,13 @@
 // Import dependencies
 import * as React from 'react'
+import { RootState } from '../reducers/rooReducer'
+import {connect} from "react-redux";
 
 // Import TodoItem
 import TodoItem from './todo-item'
 
 // Import interfaces
-import { TodoListInterface } from '../interfaces/interfaces'
+import { TodoInterface } from "../interfaces/interfaces";
 
 function computeVisible (visibilityFilter: string, isCompleted: boolean ): string {
     let className = 'show-todo-item';
@@ -15,12 +17,34 @@ function computeVisible (visibilityFilter: string, isCompleted: boolean ): strin
     return className;
 }
 
+// interface OwnProps {
+// }
+
+function mapStateToProps (state: RootState) {
+	return {
+		todoList: state.todo_reducer.slice(0),
+		visibilityFilter: state.visibility_filter_reducer
+	};
+}
+
+const mapDispatchToProps = null
+
+const connector = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)
+
+type StateProps = ReturnType<typeof mapStateToProps>;
+// type DispatchProps = typeof mapDispatchToProps;
+// type Props = StateProps & DispatchProps & OwnProps;
+type Props = StateProps;
+
 // TodoList component
-const TodoList = (props: TodoListInterface) => {
+const TodoList = (props: Props) => {
   return (
     <div className="todo-list">
       <ul>
-        {props.todos.map((todo) => (
+        {props.todoList.map((todo: TodoInterface) => (
           <li key={todo.id} className={computeVisible(props.visibilityFilter, todo.isCompleted)}>
             <TodoItem
               id={todo.id}
@@ -31,4 +55,5 @@ const TodoList = (props: TodoListInterface) => {
     </div>
   )
 }
-export default TodoList
+
+export default connector(TodoList)
