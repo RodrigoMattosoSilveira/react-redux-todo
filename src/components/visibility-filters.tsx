c059660cf123545c99cd164e0ea7d0502870eecf
Rodrigo Silveira <r3850365@gmail.com>
2020-04-25 14:05:39 -0700

    mui_visibility_buttons  -  feature: Migrated the visibility buttons to MUI

--- src/components/visibility-filters.tsx
+++ src/components/visibility-filters.tsx
@@ -1,20 +1,19 @@
 // Import external dependencies
 import * as React from 'react';
 import {connect} from 'react-redux';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 
 // Import internal dependencies
 import {RootState} from "../reducers/rootReducer";
 import { VISIBILITY_FILTER_TYPES } from '../reference/references';
 import { visibility_action_set } from "../actions/visibility-filter-actions";
 
-// Local methods
-function computeClassNames (myVisibilityFilter: string): string {
-    return 'btn ' + myVisibilityFilter;
-}
-function isDisabled (myVisibilityFilter: string, propVisibilityFilter: string ): boolean {
-    return myVisibilityFilter === propVisibilityFilter;
-}
-
+/*
+ * *****************************************************************************
+ * Start of the heart of the component
+ * *****************************************************************************
+ */
 // interface OwnProps {
 // }
 
@@ -37,28 +36,68 @@ type StateProps = ReturnType<typeof mapStateToProps>;
 type DispatchProps = typeof mapDispatchToProps;
 // type Props = StateProps & DispatchProps & OwnProps;
 type Props = StateProps & DispatchProps;
+/*
+ * *****************************************************************************
+ * End of heart of the component
+ * *****************************************************************************
+ */
 
+/*
+ * *****************************************************************************
+ * Styling
+ * *****************************************************************************
+ */
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root: {
+			'& > *': {
+				margin: theme.spacing(1),
+			},
+			'& .all-todo-items': {
+				color: 'orange',
+			},
+			'& .open-todo-items': {
+				color: 'red',
+			},
+			'& .done-todo-items': {
+				color: 'green',
+			}
+		},
+	}),
+);
 
-// TodoList component
+// Local methods
+function computeClassNames (myVisibilityFilter: string): string {
+    return 'btn ' + myVisibilityFilter ;
+}
+function isDisabled (myVisibilityFilter: string, propVisibilityFilter: string ): boolean {
+    return myVisibilityFilter === propVisibilityFilter;
+}
+
+// VisibilityFilter component
 const VisibilityFilters = (props: Props) => {
+	const classes = useStyles();
   return (
-    <div className="visibility-filter">
+    <div className={classes.root}>
         <span>Show: </span>
-        <button
-            className={computeClassNames(VISIBILITY_FILTER_TYPES.ALL)}
-            disabled={isDisabled(VISIBILITY_FILTER_TYPES.ALL, props.visibilityFilter)}
-            onClick={() => props.visibility_action_set(VISIBILITY_FILTER_TYPES.ALL)}>{VISIBILITY_FILTER_TYPES.ALL}
-        </button>
-        <button
-            className={computeClassNames(VISIBILITY_FILTER_TYPES.OPEN)}
-            disabled={isDisabled(VISIBILITY_FILTER_TYPES.OPEN, props.visibilityFilter)}
-            onClick={() => props.visibility_action_set(VISIBILITY_FILTER_TYPES.OPEN)}>{VISIBILITY_FILTER_TYPES.OPEN}
-        </button>
-        <button
-            className={computeClassNames(VISIBILITY_FILTER_TYPES.DONE)}
-            disabled={isDisabled(VISIBILITY_FILTER_TYPES.DONE, props.visibilityFilter)}
-            onClick={() => props.visibility_action_set(VISIBILITY_FILTER_TYPES.DONE)}>{VISIBILITY_FILTER_TYPES.DONE}
-        </button>
+		<Button
+			variant="outlined"
+			className={'all-todo-items'}
+			disabled={isDisabled(VISIBILITY_FILTER_TYPES.ALL, props.visibilityFilter)}
+			onClick={() => props.visibility_action_set(VISIBILITY_FILTER_TYPES.ALL)}>{VISIBILITY_FILTER_TYPES.ALL}
+		</Button>
+		<Button
+			variant="outlined"
+			className={'open-todo-items'}
+			disabled={isDisabled(VISIBILITY_FILTER_TYPES.OPEN, props.visibilityFilter)}
+			onClick={() => props.visibility_action_set(VISIBILITY_FILTER_TYPES.OPEN)}>{VISIBILITY_FILTER_TYPES.OPEN}
+		</Button>
+		<Button
+			variant="outlined"
+			className={'done-todo-items'}
+			disabled={isDisabled(VISIBILITY_FILTER_TYPES.DONE, props.visibilityFilter)}
+			onClick={() => props.visibility_action_set(VISIBILITY_FILTER_TYPES.DONE)}>{VISIBILITY_FILTER_TYPES.DONE}
+		</Button>
     </div>
   )
 }
