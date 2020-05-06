94da440843f4a18e5c3dba21483c30c153e21f47
Rodrigo Silveira <r3850365@gmail.com>
2020-05-06 14:23:51 -0700

    gh-pages  -  Updates

---
+++ precache-manifest.2c842b9ac4b5ab37c075fbcdb6757ddc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02f262699fc0a1ba6edc0a92afbdcbd",
+    "url": "/react-redux-todo/index.html"
+  },
+  {
+    "revision": "8cd7174dbe860bca3671",
+    "url": "/react-redux-todo/static/css/main.39487915.chunk.css"
+  },
+  {
+    "revision": "0fc0e22b11087f2b16d9",
+    "url": "/react-redux-todo/static/js/2.17be0023.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-redux-todo/static/js/2.17be0023.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cd7174dbe860bca3671",
+    "url": "/react-redux-todo/static/js/main.5d87f45f.chunk.js"
+  },
+  {
+    "revision": "ab2db91e059f7d084adc",
+    "url": "/react-redux-todo/static/js/runtime-main.a5aa3fa1.js"
+  }
+]);
--- src/actions/visibility-filter-actions.ts
+++
@@ -1,7 +0,0 @@
-import { VISIBILITY_FILTER_ACTIONS } from '../reference/references';
-import { VisibilityFilterActionInterface } from '../interfaces/interfaces';
-
-export const visibility_action_set = (filter: string): VisibilityFilterActionInterface => ({
-  type: VISIBILITY_FILTER_ACTIONS.SET,
-  filter: filter
-})
